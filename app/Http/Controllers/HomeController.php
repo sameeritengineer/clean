50e3a2a5738829e4a0668d433d9f1bee425f408b
Harpreet <harpreet.smartitventures@gmail.com>
2020-07-31 16:09:28 +0530

    ch

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     {
         if(auth()->user()->roles()->first()->name == "admin")
         {
-            return view('home');
+            return view('admin.dashboard');
         }
         else if(auth()->user()->roles()->first()->name == "support")
         {
